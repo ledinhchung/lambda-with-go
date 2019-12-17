c8f44f7601bfca296b7fad576edc0b120683d436
Chung Le <chungld@pascaliaasia.com>
2019-12-17 16:56:30 +0700

    Fix issue variable is declare but not use

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"io/ioutil"
-
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
@@ -10,11 +8,6 @@ import (
 // Handler is executed by AWS Lambda in the main function. Once the request
 // is processed, it returns an Amazon API Gateway response object to AWS Lambda
 func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-
-	index, err := ioutil.ReadFile("public/index.html")
-	if err != nil {
-		return events.APIGatewayProxyResponse{}, err
-	}
 
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
